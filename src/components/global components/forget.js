39f06daf87ba2e4f7d08abb52c669d437367f74e
Akhila Sai Thumma <athumma@innominds.com>
2021-12-02 10:39:15 +0530

    forgot password

--- src/components/global components/forget.js
+++ src/components/global components/forget.js
@@ -1,62 +1,62 @@
 //Importing React Component from react library
 import React, { Component, useState } from "react";
-import {resetMail} from "../../store/actions"
+import { resetMail } from "../../store/actions"
 /**
  * @author:"Madhavi itikala and Spandana"
  * @returns {Html}
  * @description Implementation of Forgot Component, If user forgot password then we redirect this page.
  */
 function Forget() {
-  const userId=window.location.pathname.split('/')[3]
-  const onSubmitPassword = () => {
-    if(password && confirmPassword){
+	const userId = window.location.pathname.split('/')[3]
+	const onSubmitPassword = () => {
+		if (password && confirmPassword) {
 
-    }
-  //   setPass(true);
-  //   setTimeout(()=>{
-  //     window.location.href = "/login"
-  //   },1000)
-  };
+		}
+		//   setPass(true);
+		//   setTimeout(()=>{
+		//     window.location.href = "/login"
+		//   },1000)
+	};
 
-  const handle=(value)=>{
-    
-    if(password==value){
-      setConfirmPassword(value)
-      setMatch('')
-    }else{
-      setMatch("password not matched")
-    }
-  }
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [match,setMatch] = useState('');
-  return (
-    <>
-      <div className="container d-flex flex-column align-items-center mt-3 p-5">
-        <div className="card w-50 d-flex flex-column align-items-center p-5">
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="New Password"
-            onChange={(e)=>setPassword(e.target.value)}
-          />
-        
-          <input
-            type="text"
-            className="form-control mb-3"
-            placeholder="Confirm Password"
-            onChange={(e)=>handle(e.target.value)}
+	const handle = (value) => {
 
-          />
-            {match?<>
-          <p style={{ fontSize: "12px", fontWeight: "bold", color: "red" }}>{match}</p>
-          </>:""}
-      
-          <button className="btn btn-primary" onClick={() => onSubmitPassword()}>
-          
-            Submit
-          </button>
-{/*           
+		if (password == value) {
+			setConfirmPassword(value)
+			setMatch('')
+		} else {
+			setMatch("password not matched")
+		}
+	}
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [match, setMatch] = useState('');
+	return (
+		<>
+			<div className="container d-flex flex-column align-items-center mt-3 p-5">
+				<div className="card w-50 d-flex flex-column align-items-center p-5">
+					<input
+						type="text"
+						className="form-control mb-3"
+						placeholder="New Password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<input
+						type="text"
+						className="form-control mb-3"
+						placeholder="Confirm Password"
+						onChange={(e) => handle(e.target.value)}
+
+					/>
+					{match ? <>
+						<p style={{ fontSize: "12px", fontWeight: "bold", color: "red" }}>{match}</p>
+					</> : ""}
+
+					<button className="btn btn-primary" onClick={() => onSubmitPassword()}>
+
+						Submit
+					</button>
+					{/*           
           {pass ? (
             <>
               <p>password changed successfully</p>
@@ -64,10 +64,10 @@ function Forget() {
           ) : (
             ""
           )} */}
-        </div>
-      </div>
+				</div>
+			</div>
 
-    </>)
+		</>)
 }
 
 export default Forget;
